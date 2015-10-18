2351d0a854c82a84db0a0389d8248521fba87c2c
Cibonay Dames <cibonaydames@Cibonays-MacBook-Pro.local>
2015-10-18 16:46:12 -0400

    .
    .

---
+++ Building-Interfaces-master/css animation/js/main.js
@@ -0,0 +1,90 @@
+//$element.velocity({
+//    width: "500px",
+//    property2: value2
+//}, {
+//    /* Velocity's default options */
+//    duration: 400,
+//    easing: "swing",
+//    queue: "",
+//    begin: undefined,
+//    progress: undefined,
+//    complete: undefined,
+//    display: undefined,
+//    visibility: undefined,
+//    loop: false,
+//    delay: false,
+//    mobileHA: true
+//});
+//
+
+
+
+
+
+
+
+$(".ball").velocity({ 
+    top: "60px",    
+    left: "160px",
+},{ 
+    duration: 2000,
+})
+.velocity({ 
+    top: "120px",    
+    left: "260px",
+},{ 
+    duration: 500,
+    easing:'linear',
+
+})
+.velocity({ 
+    top: "50px",    
+    left: "160px",
+},{ 
+    duration: 500
+})
+.velocity({ 
+    top: "10px",    
+    left: "240px",
+},{ 
+    duration: 500
+})
+;
+
+$(".ronaldo").velocity({ 
+    top: "30px",    
+    left: "130px",
+},{ 
+    delay:1500,
+    duration: 2000
+})
+
+//
+//$(".ball").velocity({ 
+//    top: "60px",    
+//  left: "160px"
+//}, 2000)
+//
+//$(".ronaldo").velocity({ 
+//    top: "60px",    
+//  left: "160px"
+//}, 2000);
+//
+//$(".ball").velocity({ 
+//    top: "120px",    
+//  left: "260px"
+//}, 1000);
+//
+//$(".ball").velocity({ 
+//    top: "15px",    
+//  left: "240px"
+//}, 500);
+
+function moveball(){
+    $(".ball").velocity({ 
+                    top: "60px",    
+                    left: "160px",
+                },{ 
+                    duration: 2000
+                })
+}
---
+++ Building-Interfaces-master/jquery/js/main.js
@@ -0,0 +1,30 @@
+$("#hello").click(function() {
+            console.log("hello");
+        });
+
+// or alert("")
+
+//$( document ).ready(function() {
+// 
+//    // Your code here.
+// 
+//});
+
+
+$("#on").click(function() {
+            $( "#hello" ).addClass( "test" );
+        });
+$("#off").click(function() {
+            $( "#hello" ).removeClass ( "test" );
+        });
+
+
+$("#loader").click(function() {
+            $.ajax({
+              url: "text.html",
+              cache: false
+            })
+              .done(function( html ) {
+                $( "#holder" ).append( html );
+              });
+        });
